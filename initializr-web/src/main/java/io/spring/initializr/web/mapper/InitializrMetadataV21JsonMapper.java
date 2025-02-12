f8634faef1a8affcea25b943ba8c667a44bacaae
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-02-12 10:35:59 +0100

    WIP

--- initializr-web/src/main/java/io/spring/initializr/web/mapper/InitializrMetadataV21JsonMapper.java
+++ initializr-web/src/main/java/io/spring/initializr/web/mapper/InitializrMetadataV21JsonMapper.java
@@ -16,8 +16,6 @@
 
 package io.spring.initializr.web.mapper;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -47,15 +45,6 @@ public class InitializrMetadataV21JsonMapper extends InitializrMetadataV2JsonMap
 	 * Create a new instance.
 	 */
 	public InitializrMetadataV21JsonMapper() {
-		this(Collections.emptyList());
-	}
-
-	/**
-	 * Create a new instance using the additional template variables.
-	 * @param additionalTemplateVariables the additional template variables
-	 */
-	public InitializrMetadataV21JsonMapper(Collection<? extends TemplateVariable> additionalTemplateVariables) {
-		super(additionalTemplateVariables);
 		this.dependenciesVariables = new TemplateVariables(
 				new TemplateVariable("bootVersion", TemplateVariable.VariableType.REQUEST_PARAM));
 	}
